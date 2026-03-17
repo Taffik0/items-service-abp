41f9018fffe73af5bf5bba465c9c29fe28d20654
Taffik <Taff1k.tpl@gmail.com>
2026-03-17 22:17:55 +0300

    refactoring enpoints

--- src/dependencies/item_dependencies.py
+++ src/dependencies/item_dependencies.py
@@ -1,4 +1,21 @@
 from fastapi import Depends
 
+from database.unit_of_work import UoW
+from src.database.dbmanager import get_conn
+
 from src.database.repository.item_repository import ItemRepository, ItemRepositoryPg
-from src.services.item_service i
+from src.services.item_service import ItemService
+
+
+def get_uow(conn=Depends(get_conn)) -> UoW:
+    return UoW(conn=conn)
+
+
+def get_item_repository(conn=Depends(get_conn)) -> ItemRepository:
+    return ItemRepositoryPg(conn=conn)
+
+
+def get_item_service(
+        item_repo=Depends(get_item_repository),
+        uow=Depends(get_uow)) -> ItemService:
+    return ItemService(uow=uow, item_repo=item_repo)
--- src/routers/items_api.py
+++ src/routers/items_api.py
@@ -6,11 +6,14 @@ from src.database import inventory_dbmanager, item_dbmanager
 from lib.fast_token import authorize_user, get_uuid_of_token, AuthToken, require_auth
 from pydantic import BaseModel
 
+from src.dependencies.item_dependencies import get_item_service
+from src.services.item_service import ItemService
+
 
 router = APIRouter()
 
 
-class ItemsBatchIn(BaseModel):
+class ItemsBulkIn(BaseModel):
     ids: list[int]
 
 
@@ -42,30 +45,34 @@ async def get_item_list_legacy(request: Request, item_ids: List[int] = Query(...
 
 @router.get("/users/my/items")
 async def get_items_of_my_user(
-    token: AuthToken = Depends(require_auth)
+    token: AuthToken = Depends(require_auth),
+    item_serv: ItemService = Depends(get_item_service)
 ):
-    pass
+    return await item_serv.get_items_of_user(token.uuid)
 
 
 @router.get("/items/{item_id}")
 async def get_item(
     item_id: int,
-    token: AuthToken = Depends(require_auth)
+    token: AuthToken = Depends(require_auth),
+    item_serv: ItemService = Depends(get_item_service)
 ):
-    pass
+    return await item_serv.get_item(item_id=item_id)
 
 
 @router.get("/users/{user_uuid}/items")
 async def get_items_of_user(
     user_uuid: UUID,
-    token: AuthToken = Depends(require_auth)
+    token: AuthToken = Depends(require_auth),
+    item_serv: ItemService = Depends(get_item_service)
 ):
-    pass
+    return await item_serv.get_items_of_user(user_uuid=user_uuid)
 
 
 @router.post("/items/bulk")
 async def get_batch_items(
-    items_batch_in: ItemsBatchIn,
-    token: AuthToken = Depends(require_auth)
+    items_bulk_in: ItemsBulkIn,
+    token: AuthToken = Depends(require_auth),
+    item_serv: ItemService = Depends(get_item_service)
 ):
-    pass
+    return await item_serv.get_item_by_ids(items_bulk_in.ids)
